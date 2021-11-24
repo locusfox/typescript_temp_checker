2ede70312289fc42e8cfb6a28bd5271dc2413597
locusfox <lucaswfox7@gmail.com>
2021-11-24 17:21:15 -0600

    setup for tomorrow's temperature code

--- out/currentTemp.js
+++ out/currentTemp.js
@@ -1,34 +1,7 @@
 "use strict";
 // This file gets the CURRENT temperature
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const readline = __importStar(require("readline"));
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 const axios = require('axios');
-// const city = "Chicago";
-let city = rl.question("What city?", (answer) => {
-});
+const city = "Chicago";
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d96a40a85f84cbee00348053d187bb47&units=imperial`;
 // This tells axios to grab the url defined above, THEN run the function "response"
 axios.get(url)
