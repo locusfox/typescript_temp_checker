c482db89ccaa705ece22e0a47c91322c3690d19d
locusfox <lucaswfox7@gmail.com>
2021-11-26 13:56:11 -0600

    functional

--- out/tomorrowTemp.js
+++ out/tomorrowTemp.js
@@ -2,12 +2,12 @@
 // This file gets TOMORROWS temp
 const axios = require('axios').default; // axios.<method> will now provide autocomplete and parameter typings
 const zipCode = 60552;
-const api = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&cnt=1&appid=d96a40a85f84cbee00348053d187bb47&units=imperial`;
+const api = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=d96a40a85f84cbee00348053d187bb47&units=imperial`;
 axios.get(api)
     .then(function weather(response) {
     // handle success
-    // console.log(response.data.list[0].main.temp);
-    console.log(`Tomorrow's temperature in ${response.data.city.name}, ${response.data.city.country} is ${Math.round(response.data.list[0].main.temp)} degrees farenheit`);
+    console.log(`Tomorrow's temperature in ${response.data.city.name}, ${response.data.city.country} is ${Math.round(response.data.list[5].main.temp)} degrees farenheit`);
+    console.log(`Date: ${response.data.list[5].dt_txt}`);
 })
     .catch(function error(error) {
     // handle error
